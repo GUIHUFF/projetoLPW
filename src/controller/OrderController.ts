f1ed6ab541fd447856cf8bba9547edbd9fdd05f0
GUIHUFF <ghuff@alunos.utfpr.edu.br>
2022-06-12 17:32:49 -0300

    pegar todos os produtos do pedido

--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -1,5 +1,6 @@
 import Order from '../model/Order';
 import { Request, Response } from 'express';
+import Product from '../model/Product';
 
 export const getOrder = async (req: Request, res: Response ) => {
   try{
@@ -14,6 +15,31 @@ export const getOrderById = async (req: Request, res: Response ) => {
   const id = req.params.id;
   try{
     const order = await Order.findById(id);
+    return {status: 200, info: order}
+  }catch(err){
+    return {status: 404, info: {mesage: 'Product not found!'} }
+  }
+}
+
+export const getOrderProductById = async (req: Request, res: Response ) => {
+  const id = req.params.id;
+  try{
+    const { _id, confirmation, table, itens, client, inProgess, isFinish, isPaid, createAt} = await Order.findById(id);
+    let itensObject = [];
+    for(let i in itens){
+      const product = await Product.findById(itens[i]);
+      itensObject.push(product);
+    }
+    const order = {
+      table,
+      itensObject,
+      client,
+      inProgess,
+      confirmation,
+      isFinish,
+      isPaid,
+      createAt
+    }
     return {status: 200, info: order}
   }catch(err){
     return {status: 404, info: {mesage: 'Product not found!'} }
--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { deleteUser, getUser, getUserById, createUser, updateUser, authenticateU
 import auth from './auth';
 import { createCategory, deleteCategory, getCategory, getCategoryById, updateCategory } from './controller/CategoryController';
 import { createProduct, deleteProduct, getProduct, getProductById, getProductCategoryById, updateProduct } from './controller/ProductController';
-import { createOrder, deleteOrder, getOrder, getOrderById, updateOrder } from './controller/OrderController';
+import { createOrder, deleteOrder, getOrder, getOrderById, updateOrder, getOrderProductById } from './controller/OrderController';
 
 const router = Router();
 
@@ -180,6 +180,15 @@ router.get('/order/', async (req: Request, res: Response) => {
 router.get('/order/:id', async (req: Request, res: Response) => {
   try{
     const resposta = await getOrderById(req, res);
+    res.status(resposta.status).json( resposta.info );
+  }catch(err){
+      res.status(500).json({ erro: err });
+  }
+});
+
+router.get('/orderAll/:id', async (req: Request, res: Response) => {
+  try{
+    const resposta = await getOrderProductById(req, res);
     res.status(resposta.status).json( resposta.info );
   }catch(err){
       res.status(500).json({ erro: err });
